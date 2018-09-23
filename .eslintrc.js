c985380e4a7c2dd7cb3f84801ed966ec7915f09a
Georgiy Bukharov <souljorje@gmail.com>
2018-09-23 13:28:00 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+// const path = require('path');
+// const jestConfig = require('./tests/.eslintrc.js');
+
+module.exports = {
+  root: true,
+  extends: [
+    // https://github.com/airbnb/javascript/blob/master/README.md#airbnb-javascript-style-guide-
+    'airbnb-base',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    'plugin:vue/essential',
+    'plugin:jest/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    // https://github.com/vuejs/eslint-plugin-vue#what-is-the-use-the-latest-vue-eslint-parser-error
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      'webpack': 'webpack.config.js',
+    },
+  },
+  // https://eslint.org/docs/user-guide/configuring#specifying-environments
+  // https://eslint.org/docs/user-guide/configuring#specifying-globals
+  globals: {
+    process: true,
+  },
+  // https://eslint.org/docs/user-guide/configuring#extending-configuration-files
+
+  // https://eslint.org/docs/user-guide/configuring#configuring-plugins
+  plugins: [
+    'vue', // required to lint *.vue files
+    'jest',
+  ],
+  // add your custom rules here
+  rules: {
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e', // for e.returnvalue
+      ],
+    }],
+    // for vuex modules
+    'no-shadow': ['error', {
+      allow: ['state'],
+    }],
+    // 'jest/prefer-to-be-null': 'error',
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      optionalDependencies: ['test/unit/index.js'],
+    }],
+    'import/core-modules': ['vue'],
+    // allow async-await
+    'generator-star-spacing': 'off',
+    'space-before-function-paren': 'off',
+    'no-mixed-operators': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'jest/no-identical-title': "error",
+    'jest/no-jest-import': "error",
+    'jest/prefer-to-be-null': "warn",
+    'jest/prefer-to-be-undefined': "warn",
+    'jest/prefer-to-have-length': "warn",
+  },
+  "overrides": [{
+    "files": ["*.vue"],
+    "rules": {
+      'max-len':0
+    },
+  }],
+};
---
+++ tests/jest.config.js
@@ -0,0 +1,30 @@
+// Configuring Jest
+// See: https://facebook.github.io/jest/docs/en/configuration.html
+module.exports = {
+  verbose: true,
+  // without this line rootDir will be `./tests`
+  rootDir: '..',
+  // without this line sometimes `jsdom` fails to run the tests:
+  // see: https://github.com/facebook/jest/issues/6766
+  testURL: 'http://localhost',
+  collectCoverage: true,
+  // collectCoverageFrom: ['**/components/**/*.{vue}'],
+  coverageDirectory: '<rootDir>/tests/coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/', '/tests/',
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleFileExtensions: [
+    'js', 'vue', 'json',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue',
+  ],
+};
